815d19df8e47f54bb658729f1c6d6c0516d3598c
Rafael Barbosa <simoesbrafael@gmail.com>
2022-04-23 10:21:16 -0300

    fix js app to do

---
+++ Checkpoint2/README.md
@@ -0,0 +1,10 @@
+To DO App
+
+1. Listar itens de uma API Fake
+    1.1 Listar todos os itens na dela 
+2. Apresentalos em tela 
+    2.1 Capturar um botão para caputurar o iten na tela 
+3. Uma função que precisam de comnunicação com o servidor. 
+    3.1 Exlcuir 
+
+    
--- Checkpoint2/app.js
+++ Checkpoint2/app.js
@@ -1,15 +1,40 @@
-let = document.getElementById("fomr");
-let = document.getElementById("input");
-let = document.getElementById("msg");
-let = document.getElementById("posts");
+const API_URL_FAKE = "https://ctd-todo-api.herokuapp.com/v1";
 
-form.addEventListener("submit", (event)) => {
-    event.preventDefault();
-    console.log("button clicked");
+//let documento = document.querySelector('body');
+//documento.addEventListener('load', function() {
+//    pedirTodosAsPublicaçõesService();
+//};    - economizar linha de código balanceando com o HTML. Essas estão sendo substituidas pelo "onload"
 
-    formValidation();
 
-});
 
-let formvalidation = () => {};
+//1. Serviços = Comunicação com o servidor. 
 
+function fetchUserData(configuracaoFetch) {
+    if(configuracaoFetch.body){
+        delete configuracaoFetch.body
+    }
+    configuracaoFetch.method='GET';
+    return fetch(`${BASE_URL_API}/users/getMe/`, configuracaoFetch)
+}
+
+
+//Listar todos 
+ function pedirTodasAsPublicacoesService() {
+     console.log('Carregou tudo!')
+
+ }
+
+}
+// 2. Efeitos do servidor = Modificação do documento HTML.
+function criarItensNaDOM() {
+
+//Listar todos
+function criarItensDOM() {
+    console.log('Criar todos os itens')
+}
+
+}
+
+// Remover um 
+function removerItenDOM() {
+}
